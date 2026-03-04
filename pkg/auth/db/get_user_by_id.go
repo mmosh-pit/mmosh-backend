55a292f590dbfc10dd2d409a9347522348a8bd16
EliasR07 <eliasalejo01@gmail.com>
2026-03-04 11:06:08 -0400

    fix: Avoid sending sessions on is-auth endpoint

--- pkg/auth/db/get_user_by_id.go
+++ pkg/auth/db/get_user_by_id.go
@@ -8,6 +8,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func GetUserById(id string) (authDomain.User, error) {
@@ -25,7 +26,11 @@ func GetUserById(id string) (authDomain.User, error) {
 		return result, err
 	}
 
-	err = collection.FindOne(*ctx, bson.D{{Key: "_id", Value: objectId}}).Decode(&result)
+	err = collection.FindOne(*ctx, bson.D{{Key: "_id", Value: objectId}}, &options.FindOneOptions{
+		Projection: map[string]any{
+			"sessions": 0,
+		},
+	}).Decode(&result)
 
 	if err == mongo.ErrNoDocuments {
 		log.Printf("No document was found with the title %s\n", id)
